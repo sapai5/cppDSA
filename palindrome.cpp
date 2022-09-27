7c855a7b0f1f12f06defc4b2a9c6d9d2a2242781
U-BSD\400221 <400221@SHS-HJ88ND2.beaverton.k12.or.us>
2022-09-27 14:01:50 -0700

    2nd commit

--- palindrome.cpp
+++ palindrome.cpp
@@ -2,15 +2,25 @@
 #include<cstring>
 using namespace std;
 
+/*
+ This program detects whether an input is a palindrome or not.
+ Author: Sahil Pai
+ Date: 9/25/22
+*/
+
 char *removeSpaces(char *palindrome)
 {
+  // remove spaces
     int i = 0, j = 0;
     while (palindrome[i])
     {
-      if (isalnum(palindrome[i]))
+      if (isalnum(palindrome[i])) {
         palindrome[j++] = palindrome[i];
-        i++;
+      }
+      i++;
     }
+    //convert to lowercase
+    palindrome[j] = '\0';
     int count = 0;
     int len = strlen(palindrome);
     for (int x = 0; x < len; x++) {
@@ -35,7 +45,8 @@ int main () {
     
     int l = 0;
     int h = strlen(pal2)-1;
-
+    
+    // check if input is a palindrome
     while(h > l){
       if(pal2[l++] != pal2[h--]){
             cout << "Not a palindrome" << endl;
