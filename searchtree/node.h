3aff347730d5bff4ccb684f626d0bc89c4a83746
sapai05 <113062329+sapai05@users.noreply.github.com>
2023-04-19 21:34:27 -0700

    Update node.h

--- searchtree/node.h
+++ searchtree/node.h
@@ -1,20 +1,25 @@
-#ifndef guard
-#define guard
-class node {
- public:
-  node(int);//constructor
-  ~node();//destructor
-  //all function prototype
-  void setLeft(node*);
-  void setRight(node*);
-  node* getLeft();
-  node* getRight();
-  void setData(int);
-  int getData();
- private:
-  node* left;//holds left node
-  node* right;//holds right node
-  int data;//holds data
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+//start of header guard
+#ifndef NODE_H
+#define NODE_H
+
+class Node {
+public:
+	Node(); //constructor
+	Node* getLeft();
+	Node* getRight(); //returns node pointer to right
+	void setLeft(Node* l);
+	void setRight(Node* r); //set pointer to right
+	int getNum(); //returns number stored in node
+	void setNum(int n);
+private:
+	Node* left; 
+	Node* right; 
+	int num; //value of number in node
 };
 
 #endif
